d70a2b1dd373094a8784bf200177d55b0cead2d0
Enderson Maia <endersonmaia@gmail.com>
2020-07-19 18:42:38 -0300

    primeiro release

---
+++ README.md
@@ -0,0 +1,120 @@
+# IBGE CNAE - Cadastro Nacional de Atividade Economica
+
+Este projeto utiliza a API do IBGE para gerar uma base de dados com o cadastro dos CNAE em formato SQL.
+
+- [IBGE CNAE - Cadastro Nacional de Atividade Economica](#ibge-cnae---cadastro-nacional-de-atividade-economica)
+  - [Carregando banco de dados](#carregando-banco-de-dados)
+  - [Tabelas](#tabelas)
+    - [Seções](#seções)
+    - [Divisões](#divisões)
+    - [Grupos](#grupos)
+    - [Classes](#classes)
+    - [Subclasses](#subclasses)
+  - [Licença](#licença)
+
+## Carregando banco de dados
+
+Você precisará dos programas `curl`, `sqlite` e `jq`.
+
+Baixe este repositório, e execute o script `./load-cnae.sh`.
+
+Segue uma consulta de teste :
+
+```shell
+$> cat<<EOF | sqlite3 -csv cnae.sqlite
+    SELECT cnae_subclasses.id, cnae_subclasses.descricao, secao.nome
+    FROM cnae_subclasses
+    JOIN cnae_secao ON (cnae_secao.id = cnae_subclasses.secao_id)
+    WHERE secao.id = 'A'
+EOF
+A,"AGRICULTURA, PECUÁRIA, PRODUÇÃO FLORESTAL, PESCA E AQÜICULTURA",0111301,"CULTIVO DE ARROZ"
+A,"AGRICULTURA, PECUÁRIA, PRODUÇÃO FLORESTAL, PESCA E AQÜICULTURA",0111302,"CULTIVO DE MILHO"
+A,"AGRICULTURA, PECUÁRIA, PRODUÇÃO FLORESTAL, PESCA E AQÜICULTURA",0111303,"CULTIVO DE TRIGO"
+A,"AGRICULTURA, PECUÁRIA, PRODUÇÃO FLORESTAL, PESCA E AQÜICULTURA",0111399,"CULTIVO DE OUTROS CEREAIS NÃO ESPECIFICADOS ANTERIORMENTE"
+A,"AGRICULTURA, PECUÁRIA, PRODUÇÃO FLORESTAL, PESCA E AQÜICULTURA",0112101,"CULTIVO DE ALGODÃO HERBÁCEO"
+```
+
+## Tabelas
+
+A hierarquia do CNAE é Seção > Divisão -> Grupo -> Classe -> Subclasse.
+
+Ex.:
+
+```
+Seção       A               Agricultura, pecuária, produção florestal, pesca e aqüicultura
+Divisão     01              Agricultura, pecuária e serviços relacionados
+Grupo       01.1            Produção de lavouras temporárias
+Classe      01.11-3         Cultivo de cereais
+Subclasse   0111-3/01       Cultivo de arroz
+```
+
+### Seções
+
+- `cnae_secoes`
+
+```
+campo       tipo
+----        ----
+id          CHAR(1)
+descricao   TEXT
+```
+
+### Divisões
+
+- `cnae_divisoes`
+
+```
+campo       tipo
+-----       ----
+id          CHAR(2)
+descricao   TEXT
+secao_id    CHAR(1)
+```
+
+### Grupos
+
+- `cnae_grupos`
+
+```
+campo       tipo
+----        ----
+id          CHAR(2)
+descricao   TEXT
+divisao_id  CHAR(2)
+secao_id    CHAR(1)
+```
+
+### Classes
+
+- `cnae_classes`
+
+```
+campo       tipo
+----        ----
+id          CHAR(5)
+descricao   TEXT
+grupo_id    CHAR(2)
+divisao_id  CHAR(2)
+secao_id    CHAR(1)
+```
+
+### Subclasses
+
+- `cnae_subclasses`
+
+```
+campo       tipo
+----        ----
+id          CHAR(5)
+descricao   TEXT
+classe_id   CHAR(5)
+grupo_id    CHAR(2)
+divisao_id  CHAR(2)
+secao_id    CHAR(1)
+```
+
+## Licença
+
+O código fonte deste projeto é [MIT License](LICENSE), Copyright (c) 2020 Enderson Tadeu Salgueiro Maia.
+
+Os dados são obtivos através da [API do IBGE](https://servicodados.ibge.gov.br/api/docs/cnae?versao=2).
---
+++ load-cnae.sh
@@ -0,0 +1,170 @@
+#!/usr/bin/env sh
+# MIT License
+
+# Copyright (c) 2020 Enderson Tadeu Salgueiro Maia
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set -eo pipefail
+[[ $TRACE ]] && set -x
+
+# https://servicodados.ibge.gov.br/api/docs/cnae?versao=2
+
+get_cnaes() {
+    echo "Baixando CNAEs do IBGE..."
+
+    curl -sSL https://servicodados.ibge.gov.br/api/v2/cnae/subclasses > "$CNAE_JSON_TMP"
+}
+
+load_cnaes() {
+    #TODO - importar campos de observações
+    echo "Ingerindo dados para tratamento..."
+
+    cat "$CNAE_JSON_TMP" \
+        | jq -cr '[.[] | {subclasse_id: .id, subclasse_descricao: .descricao, classe_id: .classe.id, classe_descricao: .classe.descricao, grupo_id: .classe.grupo.id, grupo_descricao: .classe.grupo.descricao, divisao_id: .classe.grupo.divisao.id, divisao_descricao: .classe.grupo.divisao.descricao, secao_id: .classe.grupo.divisao.secao.id, secao_descricao: .classe.grupo.divisao.secao.descricao} ] | (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv' \
+        | sqlite3 -csv -header "$DB_FILE" ".import /dev/stdin _cnaes"
+}
+
+load_secoes() {
+    echo "Criando tabela cnae_secoes..."
+
+    cat <<EOF | sqlite3 "$DB_FILE"
+    DROP TABLE IF EXISTS cnae_secoes;
+    CREATE TABLE IF NOT EXISTS cnae_secoes (
+        id          CHAR(1) PRIMARY KEY NOT NULL, 
+        descricao   TEXT NOT NULL
+    );
+    INSERT INTO cnae_secoes (id, descricao)
+    SELECT DISTINCT secao_id, secao_descricao
+    FROM _cnaes;
+EOF
+}
+
+load_divisoes() {
+    echo "Criando tabela cnae_divisoes..."
+
+    cat <<EOF | sqlite3 "$DB_FILE"
+    DROP TABLE IF EXISTS cnae_divisoes;
+    CREATE TABLE IF NOT EXISTS cnae_divisoes (
+        id          CHAR(2) PRIMARY KEY NOT NULL,
+        descricao   TEXT NOT NULL,
+        secao_id    CHAR(1) NOT NULL,
+        FOREIGN KEY(secao_id) REFERENCES cnae_secoes(id)
+    );
+    CREATE INDEX divisoes_secao_id_index ON cnae_divisoes(secao_id);
+    INSERT INTO cnae_divisoes (id, descricao, secao_id)
+    SELECT DISTINCT divisao_id, divisao_descricao, secao_id
+    FROM _cnaes;
+EOF
+}
+
+load_grupos() {
+    echo "Criando tabela cnae_grupos..."
+
+    cat <<EOF | sqlite3 "$DB_FILE"
+    DROP TABLE IF EXISTS cnae_grupos;
+    CREATE TABLE IF NOT EXISTS cnae_grupos (
+        id          CHAR(2) PRIMARY KEY NOT NULL,
+        descricao   TEXT NOT NULL,
+        divisao_id  CHAR(2) NOT NULL,
+        secao_id    CHAR(1) NOT NULL,
+        FOREIGN KEY (divisao_id) REFERENCES cnae_divisoes (id),
+        FOREIGN KEY (secao_id) REFERENCES cnae_secoes (id)
+    );
+    CREATE INDEX grupos_divisao_id_index ON cnae_grupos(divisao_id);
+    CREATE INDEX grupos_secao_id_index ON cnae_grupos(secao_id);
+    INSERT INTO cnae_grupos (id, descricao, divisao_id, secao_id)
+    SELECT DISTINCT grupo_id, grupo_descricao, divisao_id, secao_id
+    FROM _cnaes
+EOF
+}
+
+load_classes() {
+    echo "Criando tabela cnae_classes..."
+
+    cat <<EOF | sqlite3 "$DB_FILE"
+    DROP TABLE IF EXISTS cnae_classes;
+    CREATE TABLE IF NOT EXISTS cnae_classes (
+        id          CHAR(5) PRIMARY KEY NOT NULL,
+        descricao   TEXT NOT NULL,
+        grupo_id    CHAR(2) NOT NULL,
+        divisao_id  CHAR(2) NOT NULL,
+        secao_id    CHAR(1) NOT NULL,
+        FOREIGN KEY (grupo_id) REFERENCES cnae_grupos (id),
+        FOREIGN KEY (divisao_id) REFERENCES cnae_divisoes (id),
+        FOREIGN KEY (secao_id) REFERENCES cnae_secoes (id)
+    );
+    CREATE INDEX classes_grupo_id_index ON cnae_classes(grupo_id);
+    CREATE INDEX classes_divisao_id_index ON cnae_classes(divisao_id);
+    CREATE INDEX classes_secao_id_index ON cnae_classes(secao_id);
+    INSERT INTO cnae_classes (id, descricao, grupo_id, divisao_id, secao_id)
+    SELECT DISTINCT classe_id, classe_descricao, grupo_id, divisao_id, secao_id
+    FROM _cnaes;
+EOF
+}
+
+load_subclasses() {
+    echo "Criando tabela cnae_subclasses..."
+
+    cat <<EOF | sqlite3 "$DB_FILE"
+    DROP TABLE IF EXISTS cnae_subclasses;
+    CREATE TABLE IF NOT EXISTS cnae_subclasses (
+        id          CHAR(7) PRIMARY KEY NOT NULL,
+        descricao   TEXT NOT NULL,
+        classe_id   CHAR(5) NOT NULL,
+        grupo_id    CHAR(2) NOT NULL,
+        divisao_id  CHAR(2) NOT NULL,
+        secao_id    CHAR(1) NOT NULL,
+        FOREIGN KEY (grupo_id) REFERENCES cnae_grupos (id),
+        FOREIGN KEY (divisao_id) REFERENCES cnae_divisoes (id),
+        FOREIGN KEY (secao_id) REFERENCES cnae_secoes (id),
+        FOREIGN KEY (classe_id) REFERENCES cnae_secoes (id)
+    );
+    CREATE INDEX subclasses_grupo_id_index ON cnae_subclasses(grupo_id);
+    CREATE INDEX subclasses_divisao_id_index ON cnae_subclasses(divisao_id);
+    CREATE INDEX subclasses_secao_id_index ON cnae_subclasses(secao_id);
+    CREATE INDEX subclasses_classe_id_index ON cnae_subclasses(classe_id);
+    INSERT INTO cnae_subclasses (id, descricao, classe_id, grupo_id, divisao_id, secao_id)
+    SELECT DISTINCT subclasse_id, subclasse_descricao, classe_id, grupo_id, divisao_id, secao_id
+    FROM _cnaes;
+EOF
+}
+
+clean_db(){
+    sqlite3 "$DB_FILE" "DROP TABLE _cnaes;"
+    sqlite3 "$DB_FILE" "VACUUM;"
+}
+
+main() {
+    trap 'rm -f "$TMPFILE"' EXIT
+    CNAE_JSON_TMP=$(mktemp) || exit 1
+
+    DB_FILE="cnae.sqlite"
+
+    get_cnaes
+    load_cnaes
+    load_secoes
+    load_divisoes
+    load_grupos
+    load_classes
+    load_subclasses
+    clean_db
+}
+
+main "$@"
